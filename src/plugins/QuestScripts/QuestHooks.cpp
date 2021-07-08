f68cdac2c2b2690051b61250911946078dd0cca1
cressidagp <gcpigni@gmail.com>
2021-07-08 13:31:01 -0300

    Report To Goldshire: incorrect emote id.

--- src/plugins/QuestScripts/QuestHooks.cpp
+++ src/plugins/QuestScripts/QuestHooks.cpp
@@ -83,7 +83,7 @@ void OntoGoldshireComplete(Player* pPlayer, Object* pObject)
 	char msg[256];
 	snprintf((char*)msg, 256, "You are dismissed %s . ", pPlayer->GetName());
 	(TO_CREATURE(pObject))->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, msg);
-	(TO_CREATURE(pObject))->Emote(EMOTE_ONESHOT_BOW);
+	(TO_CREATURE(pObject))->Emote(EMOTE_ONESHOT_SALUTE_NOSHEATH);
 }
 
 void ZuluhedtheWhacked(Player* pPlayer, Object* pObject)
